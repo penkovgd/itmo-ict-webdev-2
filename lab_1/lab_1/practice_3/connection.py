72eaef66e1974875834389352605efa2ae949cc7
George Penkov <93640345+penkovgd@users.noreply.github.com>
2025-03-10 15:39:48 +0300

    Add practice_3

---
+++ lab_1/lab_1/practice_3/connection.py
@@ -0,0 +1,17 @@
+from sqlmodel import SQLModel, Session, create_engine
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
+db_url = getenv("DB_URL")
+engine = create_engine(db_url, echo=True)
+
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
